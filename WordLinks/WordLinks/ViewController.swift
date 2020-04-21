6b3a540a909fbb7aa7fcb0bc23c6c6b2ea82be1d
philipronnie <47910747+philipronnie@users.noreply.github.com>
2020-04-21 12:04:02 -0700

    Initial Creation of iOS Project

---
+++ WordLinks/WordLinks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordLinks
+//
+//  Created by PhilipRonnie Quiambao on 4/21/20.
+//  Copyright © 2020 spice-girls-cs194. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
