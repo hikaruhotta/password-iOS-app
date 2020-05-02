11f895b984825f3a34e5118f05ee90afabe50540
Hikaru Hotta <46637503+HikaruHotta@users.noreply.github.com>
2020-05-02 15:22:47 -0700

    Added password_prototype source code
    Code from paircoding session between Philip and Hikaruy

---
+++ password_prototype/password_prototype/WelcomeScreenButton.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeScreenButton.swift
+//  password_prototype
+//
+//  Created by Hikaru Hotta on 5/2/20.
+//  Copyright © 2020 Hikaru Hotta. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class WelcomeScreenButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBInspectable
+    var cornerRadius: CGFloat = 0 {
+        didSet{
+            setButton(cornerRadius: cornerRadius)
+        }
+    }
+    
+    func setButton(cornerRadius: CGFloat) {
+        layer.cornerRadius = cornerRadius
+    }
+    
+    
+
+}
