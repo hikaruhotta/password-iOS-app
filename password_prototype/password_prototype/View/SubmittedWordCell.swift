22307b27a77f44a2ca7bfd78b3e0c65afe3362e3
philipronnie <47910747+philipronnie@users.noreply.github.com>
2020-05-04 16:30:05 -0700

    Created SubmittedWordCell and VC

---
+++ password_prototype/password_prototype/View/SubmittedWordCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubmittedWordCell.swift
+//  password_prototype
+//
+//  Created by PhilipRonnie Quiambao on 5/4/20.
+//  Copyright © 2020 Hikaru Hotta. All rights reserved.
+//
+
+import UIKit
+
+class SubmittedWordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
