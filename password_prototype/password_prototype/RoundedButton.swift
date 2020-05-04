4430e47cd13a5be16a6de0674308ecfbf6811461
philipronnie <47910747+philipronnie@users.noreply.github.com>
2020-05-04 15:18:36 -0700

    added Round and Grad btns

---
+++ password_prototype/password_prototype/RoundedButton.swift
@@ -0,0 +1,29 @@
+//
+//  RoundedButton.swift
+//  ask_1.0
+//
+//  Created by PhilipRonnie Quiambao on 2/8/20.
+//  Copyright © 2020 PhilipRonnie Quiambao. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable
+    var cornerRadius: CGFloat = 0 {
+        didSet{
+            setButton(cornerRadius: cornerRadius)
+        }
+    }
+    
+    func setButton(cornerRadius: CGFloat){
+        if cornerRadius == 0 {
+            layer.cornerRadius = layer.frame.height / 2
+        }
+        layer.cornerRadius = cornerRadius
+    }
+
+}
