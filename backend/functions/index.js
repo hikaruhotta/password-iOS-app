e8430cccf8779477cdbab7c8475913e5f0759268
Buck Bukaty <buck.bukaty@gmail.com>
2020-05-09 15:51:22 -0700

    fix createLobby error propagation

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -41,13 +41,20 @@ exports.createLobby = functions.https.onCall((data, context) => {
                 break;
             }
         }
-        if (!freeCode) {
-            throw new functions.https.HttpsError("resource-exhausted", "Could not find a free lobby code in 1000 attempts.");
+        if (!freeCode) { // couldn't find a lobby code.
+            // throwing errors inside transaction doesn't seem to work properly with promise rejection.
+            // so just abort transaction and throw error in then() clause below.
+            return;
         }
-
+        
         let newMapping = { lobbyId: lobbyId, created: now };
         lobbyCodeMap[freeCode] = newMapping;
         return lobbyCodeMap;
+    }, (error, committed, snapshot) => {
+        if (!committed) {
+            throw new functions.https.HttpsError("resource-exhausted",
+                "Could not find a free lobby code in 1000 attempts.");
+        }
     });
 
     // wait for lobby and code to be created
@@ -55,8 +62,6 @@ exports.createLobby = functions.https.onCall((data, context) => {
         // console.log("lobbyCreation result:", values[0]);
         // console.log("lobbyCodeCreation result:", values[1]);
         return { lobbyId:lobbyId, hostSecret:hostSecret };
-    }, error => {
-        console.error(`Error when creating lobby: ${error}`);
     });
 
 });
