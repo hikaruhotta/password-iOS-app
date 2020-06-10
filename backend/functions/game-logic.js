6a179d0a6484f35d9f9c1c8bc454c6e36f6eab39
Buck Bukaty <buck.bukaty@gmail.com>
2020-06-10 16:50:42 -0700

    pull game logic out of index.js

---
+++ backend/functions/game-logic.js
@@ -0,0 +1,135 @@
+const utils = require('./utils');
+const wordUtils = require('./word-utils');
+
+function scoreTurn (lobby) {
+    const numTurnsSoFar = lobby.public.turns.length;
+    const doneTurn = lobby.public.turns[numTurnsSoFar - 1];
+    const player = lobby.public.players[doneTurn.player];
+
+    if (doneTurn.wasOthersWord) {
+        const other = lobby.public.players[doneTurn.otherId];
+        other.score += 2;
+        return;
+    }
+
+    let wasChallenged = false;
+    for (const uid in doneTurn.votes) {
+        const votedChallenge = doneTurn.votes[uid];
+        if (votedChallenge) {
+            wasChallenged = true;
+            const challenger = lobby.public.players[uid];
+            if (doneTurn.wasSubmittersWord) {
+                challenger.score += 2;
+                player.score -= 1;
+            } else {
+                challenger.score -= 1;
+                player.score -= 1;
+            }
+        }
+    }
+
+    if (!wasChallenged) {
+        if (doneTurn.wasSubmittersWord) {
+            player.score += 2;
+        } else {
+            // nothing happens
+        }
+    }
+}
+
+function startNextTurnOrEndGame (lobby) {
+    const numTurnsTaken = lobby.public.turns.length;
+    const numPlayers = lobby.public.playerOrder.length;
+    const numRounds = lobby.public.settings.numRounds;
+
+    const gameIsFinished = (numTurnsTaken === numRounds * numPlayers);
+    let newStatus;
+    if (gameIsFinished) {
+        newStatus = "DONE";
+    } else {
+        // start next turn
+        const nextPlayer = lobby.public.playerOrder[numTurnsTaken % numPlayers];
+        const now = lobby.internal.transactionTime;
+        lobby.public.turns.push({ player: nextPlayer, created: now });
+        newStatus = "SUBMISSION";
+    }
+    return newStatus;
+}
+
+exports.initializeGame = function (lobby) {
+    const playerIds = Object.keys(lobby.public.players);
+    lobby.public.playerOrder = utils.shuffleArray(playerIds);
+    lobby.public.turns = [];
+    
+    const numPlayers = playerIds.length;
+    const gameSettings = lobby.public.settings;
+    const gameWords = wordUtils.getGameWords(numPlayers, gameSettings.wordBankSize);
+    lobby.public.startWord = gameWords.startWord;
+    
+    lobby.private = {};
+    for (let i = 0; i < numPlayers; i++) {
+        const playerId = playerIds[i];
+        lobby.private[playerId] = { targetWords: gameWords.playerWordLists[i] };
+    }
+
+    lobby.internal.availableWords = gameWords.availableWords;
+    const newStatus = startNextTurnOrEndGame(lobby);
+    return newStatus;
+}
+
+exports.handleWordSubmission = function (lobby) {
+    const currTurn = lobby.public.turns[lobby.public.turns.length - 1];
+    const playerId = currTurn.player;
+    const word = currTurn.submittedWord;
+
+    let newStatus = "VOTING";
+    // check if submitted word is other player's target word
+    for (const otherId in lobby.private) {
+        if (otherId === playerId) {
+            continue;
+        }
+        if (lobby.private[otherId].targetWords.includes(word)) {
+            currTurn.wasOthersWord = true;
+            currTurn.otherId = otherId;
+            scoreTurn(lobby);
+            // TODO: generate new word for Other?
+            newStatus = startNextTurnOrEndGame(lobby);
+        }
+    }
+    
+    return newStatus;
+}
+
+exports.handleVote = function (lobby) {
+    const numVotes = Object.keys(lobby.internal.votes).length;
+    const numPlayers = lobby.public.playerOrder.length;
+
+    let newStatus;
+    const allVotesTallied = (numVotes === numPlayers - 1);
+    if (allVotesTallied) {
+        const currTurn = lobby.public.turns[lobby.public.turns.length - 1];
+        // copy internal votes into public turn object so clients can see results
+        currTurn.votes = lobby.internal.votes;
+        const targetWords = lobby.private[currTurn.player].targetWords;
+        currTurn.wasSubmittersWord = targetWords.includes(currTurn.submittedWord);
+        scoreTurn(lobby);
+
+        lobby.internal.votes = {};
+        newStatus = startNextTurnOrEndGame(lobby);
+    } else {
+        newStatus = "VOTING";
+    }
+
+    return newStatus;
+}
+
+exports.assignNewWords = function (lobby, playerId) {
+    const wordBankSize = lobby.public.settings.wordBankSize;
+    const ditchedWords = lobby.private[playerId].targetWords;
+    const freshWords = lobby.internal.availableWords.splice(0, wordBankSize);
+
+    lobby.private[playerId].targetWords = freshWords;
+    lobby.internal.availableWords = utils.shuffleArray(lobby.internal.availableWords.concat(ditchedWords));
+    
+    lobby.public.players[playerId].score -= 1;
+}
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,9 +3,8 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const validation = require('./validation');
-const utils = require('./utils');
 const lobbyUtils = require('./lobby-utils');
-const wordUtils = require('./word-utils');
+const gameLogic = require('./game-logic');
 
 /**
  * Performs a transaction on a lobby object.
@@ -34,6 +33,7 @@ async function updateLobby(lobbyId, validateLobbyFn, updateLobbyFn) {
         if (transactionError) {
             return undefined; // abort transaction
         }
+        lobby.internal.transactionTime = admin.database.ServerValue.TIMESTAMP;
         updateLobbyFn(lobby);
         return lobby;
     }).then(result => {
@@ -108,62 +108,9 @@ exports.joinLobby = functions.https.onCall(async (data, context) => {
     return { lobbyId: lobbyId };
 });
 
-function startNextTurnOrEndGame(lobby) {
-    const numTurnsTaken = lobby.public.turns.length;
-    const numPlayers = lobby.public.playerOrder.length;
-    const numRounds = lobby.public.settings.numRounds;
-
-    const gameIsFinished = (numTurnsTaken === numRounds * numPlayers);
-    if (gameIsFinished) {
-        lobby.public.status = "DONE";
-    } else {
-        // start next turn
-        const nextPlayer = lobby.public.playerOrder[numTurnsTaken % numPlayers];
-        const now = admin.database.ServerValue.TIMESTAMP;
-        lobby.public.turns.push({ player: nextPlayer, created: now });
-        lobby.public.status = "SUBMISSION";
-    }
-}
-
-function scoreTurn(lobby) {
-    const numTurnsSoFar = lobby.public.turns.length;
-    const doneTurn = lobby.public.turns[numTurnsSoFar - 1];
-    const player = lobby.public.players[doneTurn.player];
-
-    if (doneTurn.wasOthersWord) {
-        const other = lobby.public.players[doneTurn.otherId];
-        other.score += 2;
-        return;
-    }
-
-    let wasChallenged = false;
-    for (const uid in doneTurn.votes) {
-        const votedChallenge = doneTurn.votes[uid];
-        if (votedChallenge) {
-            wasChallenged = true;
-            const challenger = lobby.public.players[uid];
-            if (doneTurn.wasSubmittersWord) {
-                challenger.score += 2;
-                player.score -= 1;
-            } else {
-                challenger.score -= 1;
-                player.score -= 1;
-            }
-        }
-    }
-
-    if (!wasChallenged) {
-        if (doneTurn.wasSubmittersWord) {
-            player.score += 2;
-        } else {
-            // nothing happens
-        }
-    }
-}
-
 exports.startGame = functions.https.onCall(async (data, context) => {
-    const playerId = validation.getUid(context);
     const gameSettings = validation.getGameSettings(data);
+    const playerId = validation.getUid(context);
     const lobbyId = await lobbyUtils.findLobbyIdFromUID(playerId);
 
     const validateLobbyFn = lobby => {
@@ -180,23 +127,9 @@ exports.startGame = functions.https.onCall(async (data, context) => {
     
     const updateLobbyFn = lobby => {
         lobby.public.settings = gameSettings;
-        const playerIds = Object.keys(lobby.public.players);
-        lobby.public.playerOrder = utils.shuffleArray(playerIds);
-        lobby.public.turns = [];
         
-        const numPlayers = playerIds.length;
-        const gameWords = wordUtils.getGameWords(numPlayers, gameSettings.wordBankSize);
-        lobby.public.startWord = gameWords.startWord;
-        
-        lobby.private = {};
-        for (let i = 0; i < numPlayers; i++) {
-            const playerId = playerIds[i];
-            lobby.private[playerId] = { targetWords: gameWords.playerWordLists[i] };
-        }
-
-        lobby.internal.availableWords = gameWords.availableWords;
-        
-        startNextTurnOrEndGame(lobby);
+        let newStatus = gameLogic.initializeGame(lobby);
+        lobby.public.status = newStatus;
     };
 
     return updateLobby(lobbyId, validateLobbyFn, updateLobbyFn);
@@ -213,8 +146,7 @@ exports.submitWord = functions.https.onCall(async (data, context) => {
                 `Lobby with id ${lobbyId} not awaiting word submission.`);
         }
 
-        const turnList = lobby.public.turns;
-        const currTurn = turnList[turnList.length - 1];
+        const currTurn = lobby.public.turns[lobby.public.turns.length - 1];
         if (currTurn.player !== playerId) {
             const activePlayer = lobby.public.players[currTurn.player].displayName;
             return new functions.https.HttpsError("failed-precondition",
@@ -228,24 +160,9 @@ exports.submitWord = functions.https.onCall(async (data, context) => {
         const currTurn = lobby.public.turns[lobby.public.turns.length - 1];
         currTurn.submittedWord = word;
         lobby.public.votesTallied = 0;
-        
-        // check if submitted word is other player's target word
-        for (const otherId in lobby.private) {
-            if (otherId === playerId) {
-                continue;
-            }
-            if (lobby.private[otherId].targetWords.includes(word)) {
-                currTurn.wasOthersWord = true;
-                currTurn.otherId = otherId;
-                scoreTurn(lobby);
-                // TODO: generate new word for Other?
-                startNextTurnOrEndGame(lobby);
-                return;
-            }
-        }
-        
-        lobby.public.status = "VOTING";
-        return;
+
+        const newStatus = gameLogic.handleWordSubmission(lobby);
+        lobby.public.status = newStatus;
     };
 
     return updateLobby(lobbyId, validateLobbyFn, updateLobbyFn);
@@ -272,14 +189,7 @@ exports.requestNewWords = functions.https.onCall(async (data, context) => {
     };
 
     const updateLobbyFn = lobby => {
-        const wordBankSize = lobby.public.settings.wordBankSize;
-        const ditchedWords = lobby.private[playerId].targetWords;
-        const freshWords = lobby.internal.availableWords.splice(0, wordBankSize);
-
-        lobby.private[playerId].targetWords = freshWords;
-        lobby.internal.availableWords = utils.shuffleArray(lobby.internal.availableWords.concat(ditchedWords));
-        
-        lobby.public.players[playerId].score -= 1;
+        gameLogic.assignNewWords(lobby, playerId);
     };
 
     return updateLobby(lobbyId, validateLobbyFn, updateLobbyFn);
@@ -317,18 +227,8 @@ exports.voteOnWord = functions.https.onCall(async (data, context) => {
         lobby.internal.votes[playerId] = vote;
         lobby.public.votesTallied++;
 
-        const numVotes = Object.keys(lobby.internal.votes).length;
-        const numPlayers = lobby.public.playerOrder.length;
-        if (numVotes === numPlayers - 1) {
-            const currTurn = lobby.public.turns[lobby.public.turns.length - 1];
-            currTurn.votes = lobby.internal.votes;
-            const targetWords = lobby.private[currTurn.player].targetWords;
-            currTurn.wasSubmittersWord = targetWords.includes(currTurn.submittedWord);
-            scoreTurn(lobby);
-
-            lobby.internal.votes = {};
-            startNextTurnOrEndGame(lobby);
-        }
+        const newStatus = gameLogic.handleVote(lobby);
+        lobby.public.status = newStatus;
     };
 
     return updateLobby(lobbyId, validateLobbyFn, updateLobbyFn);
